fa4d1ef3fdecd9be2342ec2194ed27b37690a93a
Rafal Chabasinski <r.chabasinski@student.mini.pw.edu.pl>
2020-11-06 22:38:48 +0100

    fixed splash screen
    Signed-off-by: Rafal Chabasinski <r.chabasinski@student.mini.pw.edu.pl>

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:netguru/pages/home_page.dart';
-import 'package:splashscreen/splashscreen.dart';
 
 void main() {
-  runApp(new MaterialApp(
-    home: new MyApp(),
-    title: 'Netguru Core Values',
-    theme: ThemeData(
-      primarySwatch: Colors.green,
-    ),
-    darkTheme: ThemeData.dark(),
-  ));
+  runApp(TheApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class TheApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new SplashScreen(
-      seconds: 1,
-      navigateAfterSeconds: HomePage(),
-      image: new Image.asset("assets/images/netguru.png"),
-      backgroundColor: Colors.black12,
-      photoSize: 100.0,
-      loaderColor: Colors.transparent,
-      routeName: "/x",
+    return MaterialApp(
+      title: 'Netguru Core Values',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
     );
   }
 }
